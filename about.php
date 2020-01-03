5c07f3e6a7affe66fd4a0c4b9a09904d7ac70e26
Zenbook Flip UX362F <703comua2@gmail.com>
2020-01-03 22:28:40 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,3 @@
+<?php
+    echo '<h1>About page</h1>';
+    echo '<a href="index.php">Index page</a>';
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    //phpinfo();
+    $dbh = new PDO('mysql:host=my_mysql2;port=3306;dbname=public', 'root','secret');
